ad8294fd90c220e66abb897dfcf1bc888a94ffb0
Christian <christian.mig.flmo@gmail.com>
2025-11-03 12:53:26 -0600

    First commit including initial setup and folder sturcture

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div className="mt-4 rounded-md bg-red-500 px-2 py-1 text-center text-white hover:bg-red-600">
+      <p className="rounded bg-blue-300 p-2 text-lg font-bold text-white">
+        HolaTailwind!
+      </p>
+    </div>
+  );
+}
+
+export default App;
